f047a464b97568761c8c31822c09b6ff19c328ec
giuseppevignanello <peppe.vignanello@gmail.com>
2023-09-15 17:47:53 +0200

    add some comments

--- src/org/lessons/java/valutazioni/Main.java
+++ src/org/lessons/java/valutazioni/Main.java
@@ -31,26 +31,36 @@ public class Main {
 			}		
 		}
 		
+		//promoted and rejected students
 		Studente[] rejectedStudentsArray = new Studente [rejectedStudents];
 		Studente[] promotedStudentsArray = new Studente [promotedStudents];
 		
-		int rejectedIndex = 0;
-		int promotedIndex = 0;
 		Studente bestStudent = new Studente(0, 0, 0);
 		Studente worstStudent = new Studente(0, 0, 5);
 		
+		//index
+		int rejectedIndex = 0;
+		int promotedIndex = 0;
+				
 		for (int i = 0; i < students.length; i++) {
 			if(students[i].isRejected()) {
+				
+				//check if they are the worst 
 				if (students[i].avgVote < worstStudent.avgVote) {
 					worstStudent = students[i];
 				}
+				
+				//add to rejected array
 				rejectedStudentsArray[rejectedIndex] = students[i];
 				rejectedIndex ++;
 				
 			} else {
+				//check if they are the best
 				if(students[i].avgVote > bestStudent.avgVote) {
 					bestStudent = students[i];
 				}
+				
+				//add to promoted array
 				promotedStudentsArray[promotedIndex] = students[i];
 				promotedIndex ++;
 				
@@ -59,17 +69,20 @@ public class Main {
 		
 		
 		
-		
-		
+		//output
+		System.out.println("--------------------------");
 		System.out.println("There are: " + rejectedStudents + " rejected students");
+		System.out.println("--------------------------");
 		System.out.println("Promoted students: ");
 		for (int i = 0; i < promotedStudentsArray.length; i++) {
 			System.out.println(promotedStudentsArray[i].id);
 		}
+		System.out.println("--------------------------");
 		System.out.println("Rejected students: ");
 		for (int i = 0; i < promotedStudentsArray.length; i++) {
 			System.out.println(rejectedStudentsArray[i].id);
 		}
+		System.out.println("--------------------------");
 		System.out.println("The best promoted student is: " + bestStudent.id);
 		System.out.println("The worst rejected student is: " + worstStudent.id);
 	}
