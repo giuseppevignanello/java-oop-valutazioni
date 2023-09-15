3ca137ca2c94dba47b504a188d44a8e025e737a3
giuseppevignanello <peppe.vignanello@gmail.com>
2023-09-15 16:04:25 +0200

    add Studente class

--- src/org/lessons/java/valutazioni/Studente.java
+++ src/org/lessons/java/valutazioni/Studente.java
@@ -1,5 +1,30 @@
 package org.lessons.java.valutazioni;
 
 public class Studente {
-
+	
+	public int id; 
+	public int percentageOfAbsences;
+	public double avgVote;
+	
+	int maxAbsencesRejectedRange = 50;
+	int minAbsencesRejectedRange = 25; 
+	int minAvgVote = 2;
+	
+	
+	public Studente(int id, int percentageOfAbsences, double avgVote) {
+		this.id = id; 
+		this.percentageOfAbsences = percentageOfAbsences;
+		this.avgVote = avgVote;
+	}
+	
+	public boolean isRejected() {
+		if( (avgVote < minAvgVote) || 
+			(percentageOfAbsences > maxAbsencesRejectedRange) || 
+			((percentageOfAbsences > minAbsencesRejectedRange) && (avgVote <= minAvgVote))) {
+			return true;
+		} else {
+			return false;
+		}
+		
+	}
 }
